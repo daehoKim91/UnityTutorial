98c7f6b0c02569e0b0b267a642c169d89cafd0e4
daehoKim91 <typedef91@gmail.com>
2015-01-25 00:03:48 +0900

    firstCommit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate(Input.acceleration.x, 0, Input.acceleration.y);
+
+
+
+
+
+
+		/*
+		rigidbody.MovePosition(new Vector3(transform.position.x, (transform.position.y + 10f * Time.deltaTime), transform.position.z));
+		*/
+	}
+}
---
+++ Assets/insideCubeOrientation.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+	renderer.material.color = Color(0.5,1,1);
+}
+
+function Update () {
+	transform.rotation = Input.gyro.attitude;
+	Debug.Log(Input.gyro.rotationRate.x);
+	Debug.Log(Input.gyro.rotationRate.y);
+	Debug.Log(Input.gyro.rotationRate.z);
+	
+}
