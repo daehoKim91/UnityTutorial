9027e56e39216cec6d28e375951f8006e7911049
SungPhill Moon <sp9103@naver.com>
2015-01-28 12:28:36 +0900

    -속도, 가속도, 질량 구현 -Collision Detection 구현중.. 반사각도 계산 작업 진행중

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,23 +2,79 @@
 using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
+	private float tScale;
+	private Vector2 vel;
+	private Vector2 accel;
+	private float mass;
+	private float modulus;					//탄성 계수
+
+	private Vector3 prevPosition;
 
 	// Use this for initialization
 	void Start () {
+		Debug.Log ("Obj start");
+
+		tScale = 0.1f;
+		vel.x = vel.y = 0.0f;
+		mass = 1.0f;
+
+		accel.x = accel.y = 0.0f;
+
+		modulus = 0.8f;
+
+		prevPosition = this.transform.position;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		prevPosition = this.transform.position;
 
-		transform.Translate(Input.acceleration.x, 0, Input.acceleration.y);
+		//calc velocity
+		vel.x += accel.x * tScale;
+		vel.y += accel.y * tScale;
 
+		//change position
+		Vector3 tPos;
+		tPos = this.transform.position;
+		tPos.x += vel.x * tScale;
+		tPos.z += vel.y * tScale;
 
+		this.transform.position = tPos;
 
+		//change force
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			accel.y += 0.1f;
+		}
 
+		if(Input.GetKeyDown(KeyCode.LeftArrow)){
+			accel.x -= 0.1f;
+		}
 
+		if(Input.GetKeyDown(KeyCode.DownArrow)){
+			accel.y -= 0.1f;
+		}
+		
+		if(Input.GetKeyDown(KeyCode.RightArrow)){
+			accel.x += 0.1f;
+		}
 
-		/*
-		rigidbody.MovePosition(new Vector3(transform.position.x, (transform.position.y + 10f * Time.deltaTime), transform.position.z));
-		*/
+		if(Input.GetKeyDown(KeyCode.Space)){
+			accel.x = accel.y = 0.0f;
+		}
+
+		Debug.Log (accel);
+	}
+
+	void OnCollisionEnter( Collision col ) {
+		Debug.Log ("Collision Detected");
+		//transform.position = prevPosition;
+
+		vel.x *= -1.0f * modulus;
+		vel.y *= -1.0f * modulus;
+	}
+
+	void OnCollisionStay(Collision collisionInfo){
+		vel.x = vel.y = 0.0f;
+		Debug.Log ("Stay");
 	}
 }
